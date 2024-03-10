213776e27320c5a5941b2fb417fc01b81d87bde2
hamid <ec22626@qmul.ac.uk>
2024-03-10 22:14:04 +0000

    moved components to src

--- WeatherApp/routes/Home.jsx
+++ WeatherApp/routes/Home.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import "./Home.css";
-import TopSection from "../components/TopSection";
-import MidSection from "../components/MidSection";
-import BottomSection from "../components/BottomSection";
+import TopSection from "../src/components/TopSection";
+import MidSection from "../src/components/MidSection";
+import BottomSection from "../src/components/BottomSection";
 // import Battery from "../components/battery";
 // import Time from "../components/time";
 
 const Home = () => {
   return (
     <div className="Home">
-      
-        <TopSection />
+      <TopSection />
 
-        <MidSection />
+      <MidSection />
 
-        <BottomSection />
-      
+      <BottomSection />
     </div>
   );
 };
