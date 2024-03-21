e92529e983698f7c374530b2ac7cdee7eb8b7f71
hamid <ec22626@qmul.ac.uk>
2024-03-21 17:12:29 +0000

    fixed weatherDetails

--- WeatherApp/src/components/weatherdetails.jsx
+++ WeatherApp/src/components/weatherdetails.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./weatherdetails.css";
 import { setLocationInLS } from "../functions/location";
 import { API_KEY } from "../API";
-import Chart from "chart.js/auto"; 
+import Chart from "chart.js/auto";
 
 const Weatherdetails = ({ location }) => {
   // State variables for weather details
@@ -13,28 +13,27 @@ const Weatherdetails = ({ location }) => {
   const [pressure, setPressure] = useState("");
   const [wind, setWind] = useState("");
   const [precipitationData, setPrecipitationData] = useState([]);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
-  // Fetch weather details on initial mount and when 'location' changes 
+  // Fetch weather details on initial mount and when 'location' changes
   useEffect(() => {
     const fetchWeatherDetails = async () => {
       if (!location) {
-        console.log(location)
+        console.log(location);
         // Fetch details for default location (Mile End)
-        const mileEndLat = 51.5250913; 
+        const mileEndLat = 51.5250913;
         const mileEndLon = -0.0350468;
         const weatherURL = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${mileEndLat}&lon=${mileEndLon}&appid=${API_KEY}&units=metric`;
 
         try {
           const response = await fetch(weatherURL);
           const weatherData = await response.json();
-          setWeatherDetails(weatherData); 
-          setLoading(false); 
+          setWeatherDetails(weatherData);
+          setLoading(false);
         } catch (error) {
           console.log("Error fetching weather data");
-          setLoading(false); 
+          setLoading(false);
         }
-
       } else {
         // Fetch details based on user-provided location
         const GEO_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`;
@@ -50,18 +49,17 @@ const Weatherdetails = ({ location }) => {
 
             const weatherResponse = await fetch(weatherURL);
             const weatherData = await weatherResponse.json();
-            setWeatherDetails(weatherData); 
+            setWeatherDetails(weatherData);
             setLoading(false);
           }
-
         } catch (error) {
           console.log("Error fetching weather data");
-          setLoading(false); 
+          setLoading(false);
         }
       }
     };
 
-    fetchWeatherDetails(); 
+    fetchWeatherDetails();
     /*here we pass through location in the dependancy array of the use effect hook
     incase it changes and we want the effect to be re-executed*/
   }, [location]);
@@ -75,9 +73,7 @@ const Weatherdetails = ({ location }) => {
     setTempMax(firstDayData.temp.max);
     setPressure(firstDayData.pressure);
     setWind(firstDayData.speed);
-
   };
-
 
   return (
     <div className="details-container">
@@ -86,34 +82,39 @@ const Weatherdetails = ({ location }) => {
       {!loading && (
         <>
           <div className="tempBox">
-           
             <p>
-              Min Temperature: <span>{tempMin}</span>°C
+              Min Temperature:<br></br>
+              <span>{tempMin}</span>°C
             </p>
             <p>
-              Max Temperature: <span>{tempMax}</span>°C
+              Max Temperature: <br></br>
+              <span>{tempMax}</span>°C
             </p>
           </div>
           <div className="otherDetails">
             <div className="detailBox">
               <p>
-                Temperature: <span>{temp}</span>°C
+                Temperature:<br></br>
+                <span>{temp}</span>°C
               </p>
             </div>
             <div className="detailBox">
               <p>
-                Humidity: <span>{humidity}</span>%
+                Humidity: <br></br>
+                <span>{humidity}</span>%
               </p>
             </div>
 
             <div className="detailBox">
               <p>
-                Average Pressure: <span>{pressure}</span> mbar
+                Average Pressure: <br></br>
+                <span>{pressure}</span> mbar
               </p>
             </div>
             <div className="detailBox">
               <p>
-                Wind: <span>{wind}</span> mph
+                Wind: <br></br>
+                <span>{wind}</span> mph
               </p>
             </div>
           </div>
